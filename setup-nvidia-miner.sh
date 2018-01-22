6ae0d48cd977f7735931ed4650744ad09235e853
Sanjay Shah <sanjay@strongloop.com>
2018-01-22 00:38:03 -0800

    install gcc needed for nvidia

--- setup-nvidia-miner.sh
+++ setup-nvidia-miner.sh
@@ -2,6 +2,8 @@ sudo apt-get update \
 && sudo apt-get upgrade -y \
 && sudo apt-get dist-upgrade -y
 
+sudo apt-get install build-essential -y
+
 mkdir /tmp/wtf-temp && cd /tmp/wtf-temp
 git clone https://github.com/wattflare/nvidia-driver.git
 chmod +x NVIDIA-Linux-x86_64-384.111.run

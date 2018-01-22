27d6e52d7c0313b67f55bb4423e423553c46d25d
Sanjay Shah <sanjay@strongloop.com>
2018-01-21 23:37:43 -0800

    add nvidia driver git

--- setup-nvidia-miner.sh
+++ setup-nvidia-miner.sh
@@ -2,3 +2,11 @@ sudo apt-get update \
 && sudo apt-get upgrade -y \
 && sudo apt-get dist-upgrade -y
 
+mkdir /tmp/wtf-temp && cd /tmp/wtf-temp
+git clone https://github.com/wattflare/nvidia-driver.git
+chmod +x NVIDIA-Linux-x86_64-384.111.run
+./NVIDIA-Linux-x86_64-384.111.run
+cd $HOME
+
+
+
